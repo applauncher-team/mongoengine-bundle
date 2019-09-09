f5f3c0db4a3e639dd14ff1cb2badff3f2b7a4c14
Álvaro García Gómez <agarcia@bmat.com>
2019-09-09 10:43:40 +0200

    Added support for retryWrites

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as fp:
+    install_requires = fp.readlines()
+
 setup(
-  name='mongoengine_bundle',
-  packages=['mongoengine_bundle'],
-  version='1.1',
-  description='mongoengine support for applauncher',
-  author='Alvaro Garcia Gomez',
-  author_email='maxpowel@gmail.com',
-  url='https://github.com/applauncher-team/mongoengine_bundle',
-  download_url='https://github.com/applauncher-team/mongoengine_bundle/archive/master.zip',
-  keywords=['mongoengine', 'applauncher'],
-  classifiers=['Topic :: Adaptive Technologies', 'Topic :: Software Development', 'Topic :: System', 'Topic :: Utilities'],
-  install_requires=['applauncher', 'mongoengine']
+    name='mongoengine_bundle',
+    packages=find_packages(),
+    version='1.2',
+    description='mongoengine support for applauncher',
+    author='Alvaro Garcia Gomez',
+    author_email='maxpowel@gmail.com',
+    url='https://github.com/applauncher-team/mongoengine_bundle',
+    download_url='https://github.com/applauncher-team/mongoengine_bundle/archive/master.zip',
+    keywords=['mongoengine', 'applauncher'],
+    classifiers=['Topic :: Adaptive Technologies', 'Topic :: Software Development', 'Topic :: System', 'Topic :: Utilities'],
+    install_requires=install_requires,
+    
 )
